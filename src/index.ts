6fe2be6c9eec1e104dd8a55992e4c6c0cab371b6
Sapessii <nanoo2205@gmail.com>
2023-03-18 15:52:24 +0400

    fixed decimals in converttowei

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,33 @@
-import { BigNumber } from 'ethers';
 import { formatUnits } from 'ethers/lib/utils.js';
+import BigNumber from "bignumber.js";
+import { BigNumberish } from 'ethers';
 
-export const BIG_ZERO = BigNumber.from(0);
-export const BIG_TEN = BigNumber.from(10);
+export const BIG_ZERO = new BigNumber(0);
+export const BIG_TEN = new BigNumber(10);
 
 export const shortenAddress = (address: `0x${string}`, charsNumber: number) => {
   return `${address.substring(0, charsNumber)}...${address.substring(address.length - charsNumber, address.length)}`;
 };
 
-export const formatBigNumberToFixed = (numberToFormat: BigNumber, decimalsToShow: number) => {
+export const formatBigNumberToFixed = (numberToFormat: BigNumberish, decimalsToShow: number) => {
   const decimals = 18;
   const formattedString = formatUnits(numberToFormat, decimals);
   return (+formattedString).toFixed(decimalsToShow);
 };
 
 export const convertToWei = (value: string | number, decimals = 18) => {
-  try {
-    const valueBN: BigNumber = BigNumber.from(value);
-    const decimalsBN: BigNumber = getFullDecimalMultiplier(decimals);
-    const result: BigNumber = valueBN.mul(decimalsBN);
-    return result.toString();
-  } catch {
-    return '0';
-  }
+	try {
+		const valueBN = new BigNumber(value);
+		const decimalsBN = getFullDecimalMultiplier(decimals);
+		const result = valueBN.times(decimalsBN);
+		if (result.toString().includes(".")) {
+			return "0";
+		} else {
+			return result.toString();
+		}
+	} catch {
+		return "0";
+	}
 };
 
 const getFullDecimalMultiplier = (decimals: number) => {
