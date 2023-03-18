bcd58254628e68c3b2309913eaf027c33dce5b6d
Sapessii <nanoo2205@gmail.com>
2023-03-18 18:38:14 +0400

    fixed exponential in bignumber + removed unused keywords

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import { formatUnits } from 'ethers/lib/utils.js';
 import BigNumber from 'bignumber.js';
 import { BigNumberish } from 'ethers';
 
-export const BIG_ZERO = new BigNumber(0);
-export const BIG_TEN = new BigNumber(10);
+export const BIG_ZERO = BigNumber(0);
+export const BIG_TEN = BigNumber(10);
 
 export const shortenAddress = (address: `0x${string}`, charsNumber: number) => {
   return `${address.substring(0, charsNumber)}...${address.substring(address.length - charsNumber, address.length)}`;
 };
 
-export const formatBigNumberToFixed = (numberToFormat: BigNumberish, decimalsToShow: number) => {
+export const formatBigNumberToFixed = (numberToFormat: BigNumberish, decimalsToShow: number): string => {
   try {
     const decimals = 18;
     const formattedString = formatUnits(numberToFormat, decimals);
@@ -19,15 +19,15 @@ export const formatBigNumberToFixed = (numberToFormat: BigNumberish, decimalsToS
   }
 };
 
-export const convertToWei = (value: string | number, decimals = 18) => {
+export const convertToWei = (value: string | number, decimals = 18): string => {
   try {
-    const valueBN = new BigNumber(value);
+    const valueBN = BigNumber(value);
     const decimalsBN = getFullDecimalMultiplier(decimals);
     const result = valueBN.times(decimalsBN);
-    if (result.toString().includes('.')) {
+    if (result.toFixed().includes('.')) {
       return '0';
     } else {
-      return result.toString();
+      return result.toFixed();
     }
   } catch {
     return '0';
